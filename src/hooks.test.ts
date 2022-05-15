dd62d532bc8c95ec0c3a580581c8e707247903bf
Adam Dierkens <adam@dierkens.com>
2022-05-15 10:29:29 -0700

    Add rest of hooks

--- src/hooks.test.ts
+++ src/hooks.test.ts
@@ -1,4 +1,16 @@
-import { SyncBailHook, SyncHook, SyncWaterfallHook } from ".";
+import {
+  AsyncParallelBailHook,
+  AsyncParallelHook,
+  SyncBailHook,
+  SyncHook,
+  SyncLoopHook,
+  SyncWaterfallHook,
+} from ".";
+
+const delay = (time: number) =>
+  new Promise<void>((r) => {
+    setTimeout(r, time);
+  });
 
 describe("sync hook", () => {
   it("calls taps in order", () => {
@@ -67,14 +79,96 @@ describe("sync bail hook", () => {
 describe("sync waterfall hook", () => {
   it("passes values down", () => {
     const syncWaterfallHook = new SyncWaterfallHook<[string, number, number]>();
+
+    const tap1Callback = jest.fn();
+    const tap2Callback = jest.fn();
+
     syncWaterfallHook.tap("tap 1", (str, num1, num2) => {
+      tap1Callback(str, num1, num2);
       return `World ${str}`;
     });
 
     syncWaterfallHook.tap("tap 2", (str, num1, num2) => {
+      tap2Callback(str, num1, num2);
+
       return `Hello ${str}`;
     });
 
     expect(syncWaterfallHook.call("!!!", 1, 2)).toBe(`Hello World !!!`);
+    expect(tap1Callback).toBeCalledWith("!!!", 1, 2);
+    expect(tap2Callback).toBeCalledWith("World !!!", 1, 2);
+  });
+});
+
+describe("sync loop hook", () => {
+  it("calls things in order", () => {
+    const syncLoopHook = new SyncLoopHook<[string, number]>();
+    const tap1 = jest.fn().mockReturnValueOnce(true).mockReturnValueOnce(false);
+    const tap2 = jest.fn().mockReturnValueOnce(1);
+
+    syncLoopHook.tap("tap 1", tap1);
+    syncLoopHook.tap("tap 2", tap2);
+
+    const rtn = syncLoopHook.call("test", 2);
+
+    expect(tap1).toBeCalledTimes(4);
+    expect(tap2).toBeCalledTimes(2);
+
+    expect(rtn).toStrictEqual(undefined);
+  });
+});
+
+describe("async parallel hook", () => {
+  it("calls hooks at the same time", async () => {
+    const asyncParallelHook = new AsyncParallelHook<[string, string]>();
+
+    const tap1 = jest.fn();
+    const tap2 = jest.fn();
+
+    asyncParallelHook.tap("tap1", (a, b) => {
+      tap1(a, b);
+      return delay(10);
+    });
+
+    asyncParallelHook.tap("tap1", (a, b) => {
+      tap2(a, b);
+      return delay(20);
+    });
+
+    const rtn = asyncParallelHook.call("1", "2");
+    expect(tap1).toBeCalledWith("1", "2");
+    expect(tap2).toBeCalledWith("1", "2");
+
+    expect(await rtn).toBe(undefined);
+  });
+});
+
+describe("async parallel bail hook", () => {
+  it("returns the first resolved promise", async () => {
+    const asyncParallelHook = new AsyncParallelBailHook<
+      [string, string],
+      string
+    >();
+
+    const tap1 = jest.fn();
+    const tap2 = jest.fn();
+
+    asyncParallelHook.tap("tap1", async (a, b) => {
+      tap1(a, b);
+      await delay(20);
+      return "foo";
+    });
+
+    asyncParallelHook.tap("tap1", async (a, b) => {
+      tap2(a, b);
+      await delay(10);
+      return "bar";
+    });
+
+    const rtn = asyncParallelHook.call("1", "2");
+    expect(tap1).toBeCalledWith("1", "2");
+    expect(tap2).toBeCalledWith("1", "2");
+
+    expect(await rtn).toBe("bar");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,38 @@
-export type HookRegistration = Symbol;
+export interface Tap<Args extends any[], ReturnType> {
+  key: Symbol;
+  name: string;
+  callback: (...args: Args) => ReturnType;
+}
 
-interface SyncBaseHook<Args extends any[], ReturnType> {
-  tap(name: string, callback: (...args: Args) => ReturnType): HookRegistration;
+interface SyncBaseHookType<Args extends any[], ReturnType> {
+  tap(
+    name: string,
+    callback: (...args: Args) => ReturnType
+  ): Tap<Args, ReturnType>;
   call(...args: Args): void;
-  untap(key: HookRegistration): void;
+  untap(key: Tap<Args, ReturnType>): void;
 }
 
 class Hook<Args extends any[], ReturnType>
-  implements SyncBaseHook<Args, ReturnType>
+  implements SyncBaseHookType<Args, ReturnType>
 {
-  protected taps: Array<{
-    key: HookRegistration;
-    name: string;
-    callback: (...args: Args) => ReturnType;
-  }> = [];
+  protected taps: Array<Tap<Args, ReturnType>> = [];
 
   constructor() {}
 
   public tap(
     name: string,
     callback: (...args: Args) => ReturnType
-  ): HookRegistration {
+  ): Tap<Args, ReturnType> {
     const key = Symbol(name);
-    this.taps.push({
+    const tap: Tap<Args, ReturnType> = {
       key,
       name,
       callback,
-    });
-    return key;
+    };
+    this.taps.push(tap);
+
+    return tap;
   }
 
   public call(...args: Args) {
@@ -36,8 +41,8 @@ class Hook<Args extends any[], ReturnType>
     });
   }
 
-  public untap(key: HookRegistration) {
-    this.taps = this.taps.filter((t) => t.key !== key);
+  public untap(tap: Tap<Args, ReturnType>) {
+    this.taps = this.taps.filter((t) => t.key !== tap.key);
   }
 }
 
@@ -47,19 +52,6 @@ export class SyncBailHook<Args extends any[], ReturnType> extends Hook<
   Args,
   ReturnType | undefined | null
 > {
-  public tap(
-    name: string,
-    callback: (...args: Args) => ReturnType | undefined | null
-  ): HookRegistration {
-    const key = Symbol(name);
-    this.taps.push({
-      key,
-      name,
-      callback,
-    });
-    return key;
-  }
-
   public call(...args: Args): ReturnType | undefined | null {
     for (let tapIndex = 0; tapIndex < this.taps.length; tapIndex += 1) {
       const rtn = this.taps[tapIndex].callback(...args);
@@ -82,4 +74,78 @@ export class SyncWaterfallHook<Args extends any[]> extends Hook<Args, Args[0]> {
   }
 }
 
-export class SyncLoopHook {}
+export class SyncLoopHook<Args extends any[]> extends Hook<Args, void> {
+  public call(...args: Args) {
+    let finished = false;
+
+    while (finished !== true) {
+      finished = true;
+      for (let tapIndex = 0; tapIndex < this.taps.length; tapIndex += 1) {
+        const rtn = this.taps[tapIndex].callback(...args);
+
+        if (rtn !== undefined) {
+          finished = false;
+          break;
+        }
+      }
+    }
+  }
+}
+
+export class AsyncParallelHook<Args extends any[]> extends Hook<
+  Args,
+  Promise<void>
+> {
+  public async call(...args: Args): Promise<void> {
+    await Promise.allSettled(this.taps.map((tap) => tap.callback(...args)));
+  }
+}
+
+export class AsyncParallelBailHook<Args extends any[], ReturnType> extends Hook<
+  Args,
+  Promise<ReturnType>
+> {
+  public async call(...args: Args): Promise<ReturnType> {
+    return Promise.any(this.taps.map((tap) => tap.callback(...args)));
+  }
+}
+
+export class AsyncSeriesHook<Args extends any[]> extends Hook<
+  Args,
+  Promise<void>
+> {
+  public async call(...args: Args): Promise<void> {
+    for (let tapIndex = 0; tapIndex < this.taps.length; tapIndex += 1) {
+      await this.taps[tapIndex].callback(...args);
+    }
+  }
+}
+
+export class AsyncSeriesBailHook<Args extends any[], ReturnType> extends Hook<
+  Args,
+  Promise<ReturnType>
+> {
+  public async call(...args: Args): Promise<ReturnType | undefined | null> {
+    for (let tapIndex = 0; tapIndex < this.taps.length; tapIndex += 1) {
+      const rtn = await this.taps[tapIndex].callback(...args);
+      if (rtn !== undefined) {
+        return rtn;
+      }
+    }
+  }
+}
+
+export class AsyncSeriesWaterfallHook<
+  Args extends any[],
+  ReturnType
+> extends Hook<Args, Promise<ReturnType>> {
+  public async call(...args: Args): Promise<ReturnType> {
+    let [rtn, ...rest] = args;
+
+    for (let tapIndex = 0; tapIndex < this.taps.length; tapIndex += 1) {
+      rtn = await (this.taps[tapIndex].callback as any)(rtn, ...rest);
+    }
+
+    return rtn;
+  }
+}
