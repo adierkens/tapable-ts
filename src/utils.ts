97a05cc6c9cfa2d78693107e8593b29ee2028069
Adam Dierkens <adierkens@users.noreply.github.com>
2022-12-22 09:06:31 -0800

    Add auto, eslint, prettier, circle, etc (#6)
    Add auto, eslint, prettier, etc

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,14 @@
 /**
  * Checks if `value` is equal to `check` if `check` is a string or in `check` if check is an Array
- * @param value the value being searched for
- * @param check the values to check against
+ *
+ * @param value - the value being searched for
+ * @param check - the values to check against
  * @returns `boolean`
  */
-export function equalToOrIn(value: string, check: string | Array<string>){
-  if(Array.isArray(check)){
-    return check.includes(value)
-  } else {
-    return check === value
+export function equalToOrIn(value: string, check: string | Array<string>) {
+  if (Array.isArray(check)) {
+    return check.includes(value);
   }
+
+  return check === value;
 }
